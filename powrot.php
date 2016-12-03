40ae57b026fa5f148ffcf8e1f9f1c283a2c8c042
micmar005 <michal.marczewski1@gmail.com>
2016-12-04 00:47:00 +0100

    Delete powrot.php

--- powrot.php
+++
@@ -1,8 +0,0 @@
-<?php
-$cookie_name = "user";
-$ciasto = $_COOKIE[$cookie_name];
-$cookie_name2 = "cur_dir";
-$cookie_value2 = "/home/miszumisz/ftp/z7/"."$ciasto";
-setcookie($cookie_name2, $cookie_value2, time() + (86400 * 30), "/");
-header("Location: zalogowany_klient.php");
-?>
